5e18866bd4e7dfec4171b3d2645adc7283406ef1
tal-dev <talgat.abykeev@gmail.com>
2020-05-23 01:47:37 -0500

    refactored design dropdown functionality

--- js/script.js
+++ js/script.js
@@ -11,22 +11,68 @@ const colorDropdown = shirtBlock.querySelector('#color');
 const colorDropdownList = colorDropdown.children;
 colorDiv.style.display = 'none';
 
-
-function hideOptions(list, text) {
-    for(let i = 0; i < list.length; i+=1) {
-        if(list[i].text.includes(text)) {
-            list[i].style.display = 'none';
-        }
+// Function to show or hide color dropdown
+function displayColor(flag) {
+    if (flag === 'on') {
+        colorDiv.style.display = '';
+    } else if (flag === 'off') {
+        colorDiv.style.display = 'none';
     }
 };
 
-designDropdown.addEventListener('change', (e) => {
-    if(e.target.value === 'js puns') {
-        colorDiv.style.display = '';
-        hideOptions(colorDropdownList, 'JS Puns shirt only');
+
+// Function to hide or show color options based on design selected
+function hideOptions(theme) {
+    if (theme === 'js puns') {
+       colorDropdownList[3].style.display = 'none';
+       colorDropdownList[4].style.display = 'none';
+       colorDropdownList[5].style.display = 'none';
+
+       colorDropdownList[0].style.display = '';
+       colorDropdownList[1].style.display = '';
+       colorDropdownList[2].style.display = '';
     }
-    else if()
+    if(theme === 'heart js'){
+        colorDropdownList[0].style.display = 'none';
+        colorDropdownList[1].style.display = 'none';
+        colorDropdownList[2].style.display = 'none';
+
+        colorDropdownList[3].style.display = '';
+        colorDropdownList[4].style.display = '';
+        colorDropdownList[5].style.display = '';
+    }
+};
+
+// Event to display color options based on design option
+designDropdown.addEventListener('change', (e) => {
+    if (e.target.value === 'js puns') {
+        displayColor('on');
+        hideOptions('js puns');
+        colorDropdown.selectedIndex = '0';
+        designDropdown.addEventListener('change', (e) => {
+            if (e.target.value === 'select theme') {
+                displayColor('off');
+            }
+        })
+
+    }
+    if(e.target.value === 'heart js') {
+        displayColor('on');
+        hideOptions('heart js');
+        colorDropdown.selectedIndex = '3';
+        designDropdown.addEventListener('change', (e) => {
+            if (e.target.value === 'select theme') {
+                displayColor('off');
+            }
+        })
+    }
+
+    
+
 });
+
+
+
 
 
 
@@ -37,7 +83,7 @@ function formfocus() {
 window.onload = formfocus;
 
 
-// Function to create an additional text box if 'Other' option is selected from Job Role dropdown
+// Even to create an additional text box if 'Other' option is selected from Job Role dropdown
 jobRoleDropdown.addEventListener('change', (e) => {
     if (e.target.value === 'other') {
         let input = document.createElement('input');
@@ -52,5 +98,3 @@ jobRoleDropdown.addEventListener('change', (e) => {
         });
     }
 });
-
-
